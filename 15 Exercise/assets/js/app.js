243c3f5e67bea6127cc5292a8778f01c35344e05
User <user@Users-MacBook-Pro.local>
2024-12-07 09:37:15 +0700

    jQuery: Update

--- 15 Exercise/assets/js/app.js
+++ 15 Exercise/assets/js/app.js
@@ -1,15 +1,5 @@
 "use strict";
 
 $(document).ready(function () {
-  $("input").on("keyup", function () {
-    if ($("input").val() === "") {
-      $(this).addClass("border border-danger");
-
-      $(this).removeClass("border border-success");
-    } else {
-      $(this).removeClass("border border-danger");
-
-      $(this).addClass("border border-success");
-    }
-  });
+  $("input").addClass("shadow-none");
 });
