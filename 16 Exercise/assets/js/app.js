68b38f1408440b2cc66f040a00acce835f56a8fb
zawlinn-vlog <zawlinn.profile@outlook.com>
2024-12-08 22:50:04 +0700

    jQuery: Update

--- 16 Exercise/assets/js/app.js
+++ 16 Exercise/assets/js/app.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function () {
   const getEl = document.querySelector("#fontawesome");
-  $("button").on("click", function () {
+  $("#copy").on("click", function () {
     // getEl.select();
 
     console.log(navigator);
@@ -15,6 +15,51 @@ $(document).ready(function () {
     console.log(navigator.clipboard);
 
     $(this).text("Copied!");
+  });
+
+  $("#step-01, #step-02").slideUp();
+
+  $("#btn-01, #btn-02").click(function () {
+    $(this).next().slideToggle("slow");
+
+    $(this).find(".icon").toggleClass("rotate");
+  });
+
+  //
+
+  let count = 1;
+  let maxCount = 4;
+
+  let mainContainer = document.querySelector("form");
+
+  $("#btn-add").on("click", function () {
+    if (count <= maxCount) {
+      const html = `<div class="form-group mt-4" id=el-${count}>
+                <div class="row">
+                  <div class="col">     
+                    <input type="text" name="" id="" class="form-control" placeholder="09 xxx xxx xxx" />
+                  </div>
+                  <div class="col">
+                    <button class="btn btn-danger" id="btn-del" type="button">Del</button>
+                  </div>
+                </div>
+              </div>`;
+      mainContainer.insertAdjacentHTML("beforeend", html);
+      // $("form").append(html);
+
+      count++;
+
+      if (count == 5) {
+        $(this).attr("disabled", true);
+      }
+    }
+  });
+
+  $("#addPhone").on("click", "#btn-del", function (e) {
+    this.closest(".form-group").remove();
+    count--;
+
+    $("#btn-add").attr("disabled", false);
   });
 
   //
