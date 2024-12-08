7c6a5827dffd27c6f538375c23584020d2b2df7e
User <user@Users-MacBook-Pro.local>
2024-12-08 10:02:44 +0700

    jQuery: Update

--- 16 Exercise/assets/js/app.js
+++ 16 Exercise/assets/js/app.js
@@ -1,21 +1,33 @@
 "use strict";
 
 $(document).ready(function () {
-  $(".btn").on("click", function () {
-    $(".list-group")
-      .children()
-      .each(function (int) {
-        console.log($(this).text());
-      });
+  $("button").on("click", function () {
+    $("#fontawesome").select();
 
-    $(".list-group-item").each(function () {
-      let getInd = $(this).index();
-      let getText = $(this).text();
-      let getTitle = $(this).attr("title");
+    navigator.clipboard.writeText($("#fontawesome").val());
 
-      console.log(
-        `Index is : ${getInd} and text is ${getText} and title is ${getTitle}.`
-      );
-    });
+    console.log(navigator.clipboard);
+
+    $(this).text("Copied!");
   });
 });
+
+// $(document).ready(function () {
+//   $(".btn").on("click", function () {
+//     $(".list-group")
+//       .children()
+//       .each(function (int) {
+//         console.log($(this).text());
+//       });
+
+//     $(".list-group-item").each(function () {
+//       let getInd = $(this).index();
+//       let getText = $(this).text();
+//       let getTitle = $(this).attr("title");
+
+//       console.log(
+//         `Index is : ${getInd} and text is ${getText} and title is ${getTitle}.`
+//       );
+//     });
+//   });
+// });
